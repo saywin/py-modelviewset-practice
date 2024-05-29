8bcf9a0fdea9e71b279d48a50e999863bd8868bd
tiway <stilon670@gmail.com>
2024-05-29 12:29:12 +0300

    Solution

--- author/urls.py
+++ author/urls.py
@@ -6,7 +6,7 @@ from author.views import AuthorViewSet
 app_name = "author"
 
 router = routers.DefaultRouter()
-router.register("author", AuthorViewSet)
+router.register("author", AuthorViewSet, basename="manage")
 
 urlpatterns = [
     path("", include(router.urls))
